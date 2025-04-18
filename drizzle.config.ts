4d2bdcc6b737223de0dfb80c236749b53bc0122b
Cilicia Uzziel Perez <uzzie.perez@cern.ch>
2025-04-18 04:58:55 +0200

    db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
@@ -7,8 +10,10 @@ if (!process.env.DATABASE_URL) {
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL || "",
   },
+  verbose: true,
+  strict: true,
 });
